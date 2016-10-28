b58461d6bf6ac96801612f8d4a442574db15eb7a
Siddharth <sid.st99@gmail.com>
2016-10-28 12:02:13 +0530

    Add Fast Input and Output Classes

---
+++ src/Codechef/IOUtils.java
@@ -0,0 +1,10 @@
+package Codechef;
+
+class IOUtils {
+	 public static int[] readIntArray(InputReader in, int size) {
+		int[] array = new int[size];
+		for (int i = 0; i < size; i++)
+			array[i] = in.readInt();
+		return array;
+	}
+}
