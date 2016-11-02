81d42a9faeea674a41f50e889cdcf47ef0d8340a
Siddharth <sid.st99@gmail.com>
2016-11-02 23:39:08 +0530

    Add function to print an integer array to IOUtils class

--- src/Codechef/IOUtils.java
+++ src/Codechef/IOUtils.java
@@ -6,5 +6,10 @@ class IOUtils {
 		for (int i = 0; i < size; i++)
 			array[i] = in.readInt();
 		return array;
-	}
+	 }
+	 public static void printIntArray(OutputWriter out, int[] a) {
+		 for(int i = 0 ; i < a.length ; i++) {
+			 out.print(a[i]+" ");
+		 }
+	 }
 }
